62a6270256f8a51ed5aa24ddc8523fe25ae9a0a7
TomTroppmann <troppmann.tom@gmail.com>
2022-11-20 15:03:30 +0100

    Fixes time manipulation while playing

--- src/plugins/lidar/plugin.rs
+++ src/plugins/lidar/plugin.rs
@@ -18,12 +18,13 @@ impl Plugin for LidarPlugin {
 }
 #[derive(Resource)]
 pub struct PlayerState {
-    start_time: f64,
+    pub start_time: f64,
     pub sequence: Option<Sequence>,
     mesh: Option<Handle<Mesh>>,
     pub actual_frame: usize,
-    pub start_frame: usize, 
-    has_frame_changes: bool,
+    pub start_frame: usize,
+    last_rendered_frame: usize, 
+    pub drag_paused: bool,
     pub paused: bool,
     pub fullscreen: bool,
 }
@@ -37,7 +38,8 @@ impl Default for PlayerState {
             mesh: None,
             actual_frame: 0,
             start_frame: 0,
-            has_frame_changes: false,
+            last_rendered_frame: usize::MAX,
+            drag_paused: false,
             paused: true,
             fullscreen: false,
         }
@@ -48,21 +50,17 @@ impl PlayerState {
     pub fn update(&mut self, time_in_seconds: f64){
         if let Some(sequence) = &self.sequence{
             let passed_time = time_in_seconds - self.start_time;
-            let old_index = self.actual_frame; 
             self.actual_frame = ((passed_time * 10.0) as usize + self.start_frame).min(sequence.frame_count - 1).max(0);
-            self.has_frame_changes = old_index != self.actual_frame;
         }
     }
     pub fn toggle_play(&mut self, time_in_seconds: f64){
         self.paused = !self.paused;
-        println!("{}", self.paused);
         self.start_frame = self.actual_frame;
         self.start_time = time_in_seconds;
     }
     pub fn request_frame(&mut self, frame: usize, time_in_seconds: f64){
         self.start_frame = frame;
         self.start_time = time_in_seconds;
-        self.has_frame_changes = true;
     }
 }
 
@@ -76,27 +74,7 @@ fn init_sequence(
     state.sequence = Some(read_sequence_from_dir(path).unwrap());
     state.start_time = time.elapsed_seconds_f64();
     state.start_frame = 0;
-    state.has_frame_changes = true;
     state.mesh = Some(meshes.add(Mesh::from(shape::Cube { size: 0.04 })))
-}
-
-fn spawn_frame(commands: &mut Commands, frame: &Frame, mesh: Handle<Mesh>) {
-    commands.spawn((
-        mesh,
-        SpatialBundle::VISIBLE_IDENTITY,
-        InstanceMaterialData(
-            frame
-                .0
-                .iter()
-                .map(|point| InstanceData {
-                    position: point.position,
-                    scale: 1.0,
-                    color: Color::rgb_u8(247, 127, 0).as_linear_rgba_f32(),
-                })
-                .collect(),
-        ),
-        NoFrustumCulling,
-    ));
 }
 
 fn player(
@@ -105,11 +83,10 @@ fn player(
     mut state: ResMut<PlayerState>,
     query: Query<Entity, With<InstanceMaterialData>>,
 ) {
-    if !state.paused{
+    if !state.paused && !state.drag_paused{
         state.update(time.elapsed_seconds_f64());
     }
-    
-    if state.has_frame_changes {
+    if state.last_rendered_frame != state.actual_frame {
         query.for_each(|entity| commands.entity(entity).despawn());
         if let Some(sequence) = &state.sequence {
             spawn_frame(
@@ -118,6 +95,22 @@ fn player(
                 state.mesh.as_ref().unwrap().clone(),
             )
         }
-        state.has_frame_changes = false;
+        state.last_rendered_frame = state.actual_frame;
     }
+    
+}
+
+fn spawn_frame(commands: &mut Commands, frame: &Frame, mesh: Handle<Mesh>) {
+    commands.spawn((
+        mesh,
+        SpatialBundle::VISIBLE_IDENTITY,
+        InstanceMaterialData(
+            frame.0.iter().map(|point| InstanceData {
+                    position: point.position,
+                    scale: 1.0,
+                    color: Color::rgb_u8(247, 127, 0).as_linear_rgba_f32(),
+                }).collect(),
+        ),
+        NoFrustumCulling,
+    ));
 }
--- src/plugins/ui.rs
+++ src/plugins/ui.rs
@@ -18,16 +18,25 @@ fn control_bar(
     mut windows: ResMut<Windows>
 ) {
     egui::TopBottomPanel::bottom("Playbar").show(egui_context.ctx_mut(), |ui| {
+        let elapsed = time.elapsed_seconds_f64();
         ui.horizontal(|ui| {
             if ui.add_sized(bevy_egui::egui::Vec2::new(100.0,20.0), egui::Button::new(if player.paused {"Play"} else {"Paused"})).clicked() {
-                player.toggle_play(time.elapsed_seconds_f64())
+                player.toggle_play(elapsed)
             }
             let label_space = 150.0; 
             ui.spacing_mut().slider_width = ui.available_width() - label_space;
             let max_frame = player.sequence.as_ref().map_or(0, |s| s.frame_count - 1).max(0);
-            if ui.add(egui::Slider::new(&mut player.actual_frame, 0..=max_frame).show_value(false)).changed(){
+            let slider_response = ui.add(egui::Slider::new(&mut player.actual_frame, 0..=max_frame).show_value(false));
+            if slider_response.drag_started(){
+                player.drag_paused = true;
+            }
+            if slider_response.drag_released(){
+                player.drag_paused = false;
+                player.start_time = elapsed;
+            }
+            if slider_response.changed(){
                 let frame = player.actual_frame;
-                player.request_frame(frame, time.elapsed_seconds_f64())
+                player.request_frame(frame, time.elapsed_seconds_f64());
             }
             ui.add_sized(bevy_egui::egui::Vec2::new(40.0,20.0), egui::Label::new(player.actual_frame.to_string()));
             if ui.add_sized(bevy_egui::egui::Vec2::new(100.0,20.0), egui::Button::new(if player.fullscreen {"Window"} else {"Fullscreen"})).clicked() {
