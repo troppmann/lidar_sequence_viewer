4fc48151f065e0f2d346f94102c41578d8d6ebc6
TomTroppmann <troppmann.tom@gmail.com>
2023-04-15 20:20:32 +0200

    Single executeable
    Removes unnecessary bevy features

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ pub mod math;
 pub mod plugins;
 
 use bevy::prelude::*;
+use bevy_embedded_assets::EmbeddedAssetPlugin;
 use plugins::*;
 
 fn main() {
     App::new()
         .add_startup_system(setup_window)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .build()
+                .add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin),
+        )
         .add_plugin(ConfigPlugin)
         .add_plugin(LidarPlugin)
         .add_plugin(FpsWindowTitlePlugin)
@@ -17,7 +22,7 @@ fn main() {
         .run();
 }
 
-fn setup_window(mut query_window: Query<&mut Window>){
+fn setup_window(mut query_window: Query<&mut Window>) {
     let mut window = query_window.single_mut();
     window.title = "Lidar Viewer".to_string();
     window.position = WindowPosition::Centered(MonitorSelection::Current);
